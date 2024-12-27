73b7a43c9e5f3741f9ee930df728b3013226ac18
Pratik Borse <93337756+Zepar99@users.noreply.github.com>
2024-12-27 13:39:08 +0000

    Variables and Datatypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,13 @@
+const account_Id = 71233
+let account_Email= "pratik@google.com"
+var account_Password = "12345"
+account_City = "Pune" 
+let account_State;
+//account_Id = 2 not allowed
+
+account_Email ="p@p.com"
+account_Password ="12124"
+account_City= "dhule"
+account_State = "maharastra"
+console.log(account_Id);
+console.table([account_Email,account_Id,account_Password, account_City ,account_State])
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict" // treat all JS code as per newer version
+
+//alert(3+2) we are using node.js not browser
+
+console.log(3+1)
+
+let name = "pratik"
+let age = "24"
+let isLoggedIn = false
+
+/*
+number
+bigint
+string
+bool
+null-> standalone value (its a object type)
+undefined-> value not asigned yet
+symbol -> unique
+object -> very IMP
+*/
+console.log(typeof age)
